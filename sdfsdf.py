13a8ba510d5a88960a8718c362112238bcd4c8e4
Ruslan12232 <minibig531@gmail.com>
2026-03-06 09:32:50 +0300

    Add files via upload

---
+++ sdfsdf.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if 0 < n <= 20:
+    if n == 0:
+        f1 = 0
+    else:
+        f0, f1 = 0, 1
+        for i in range(2, n + 1):
+            f0, f1 = f1, f0 + f1
+    print(f"f({n}) = {f1}")
+else:
+    print("Не пиши больше 20, ок?")
